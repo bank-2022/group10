0d113935d10dc1674ac91088418eb3e21f96d099
t1sasa03 <t1sasa03@students.oamk.fi>
2022-03-29 13:13:41 +0300

    tietokanta luotu ja dokumentit lisätty

---
+++ bank_dump.sql
@@ -0,0 +1,167 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: bank
+-- ------------------------------------------------------
+-- Server version	8.0.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `asiakas`
+--
+
+DROP TABLE IF EXISTS `asiakas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `asiakas` (
+  `id_asiakas` int unsigned NOT NULL AUTO_INCREMENT,
+  `nimi` varchar(45) NOT NULL,
+  `osoite` varchar(45) NOT NULL,
+  `puhelinnumero` varchar(45) NOT NULL,
+  PRIMARY KEY (`id_asiakas`),
+  UNIQUE KEY `id_asiakas_UNIQUE` (`id_asiakas`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `asiakas`
+--
+
+LOCK TABLES `asiakas` WRITE;
+/*!40000 ALTER TABLE `asiakas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `asiakas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `asiakastili`
+--
+
+DROP TABLE IF EXISTS `asiakastili`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `asiakastili` (
+  `id_asiakas` int unsigned NOT NULL,
+  `id_tili` int unsigned NOT NULL,
+  PRIMARY KEY (`id_tili`,`id_asiakas`),
+  KEY `id_asiakas_idx` (`id_asiakas`),
+  CONSTRAINT `id_asiakas_asiakastili` FOREIGN KEY (`id_asiakas`) REFERENCES `asiakas` (`id_asiakas`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `id_tili_asiakastili` FOREIGN KEY (`id_tili`) REFERENCES `tili` (`id_tili`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `asiakastili`
+--
+
+LOCK TABLES `asiakastili` WRITE;
+/*!40000 ALTER TABLE `asiakastili` DISABLE KEYS */;
+/*!40000 ALTER TABLE `asiakastili` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `kortti`
+--
+
+DROP TABLE IF EXISTS `kortti`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `kortti` (
+  `id_kortti` int unsigned NOT NULL AUTO_INCREMENT,
+  `korttinumero` int unsigned DEFAULT NULL,
+  `pin` varchar(255) DEFAULT NULL,
+  `id_tili` int unsigned NOT NULL,
+  `id_asiakas` int unsigned NOT NULL,
+  PRIMARY KEY (`id_kortti`),
+  UNIQUE KEY `id_kortti_UNIQUE` (`id_kortti`),
+  UNIQUE KEY `korttinro_UNIQUE` (`korttinumero`),
+  KEY `fk_kortti_tili1_idx` (`id_tili`),
+  KEY `fk_kortti_asiakas1_idx` (`id_asiakas`),
+  CONSTRAINT `id_asiakas_kortti` FOREIGN KEY (`id_asiakas`) REFERENCES `asiakas` (`id_asiakas`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `id_tili_kortti` FOREIGN KEY (`id_tili`) REFERENCES `tili` (`id_tili`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `kortti`
+--
+
+LOCK TABLES `kortti` WRITE;
+/*!40000 ALTER TABLE `kortti` DISABLE KEYS */;
+/*!40000 ALTER TABLE `kortti` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tili`
+--
+
+DROP TABLE IF EXISTS `tili`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tili` (
+  `id_tili` int unsigned NOT NULL AUTO_INCREMENT,
+  `tilinumero` varchar(45) DEFAULT NULL,
+  `saldo` float DEFAULT NULL,
+  PRIMARY KEY (`id_tili`),
+  UNIQUE KEY `id_tili_UNIQUE` (`id_tili`),
+  UNIQUE KEY `tilinumero_UNIQUE` (`tilinumero`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tili`
+--
+
+LOCK TABLES `tili` WRITE;
+/*!40000 ALTER TABLE `tili` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tili` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tilitapahtumat`
+--
+
+DROP TABLE IF EXISTS `tilitapahtumat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tilitapahtumat` (
+  `id_tilitapahtumat` int unsigned NOT NULL AUTO_INCREMENT,
+  `paivays` datetime DEFAULT NULL,
+  `tapahtuma` varchar(45) DEFAULT NULL,
+  `summa` float DEFAULT NULL,
+  `id_kortti` int unsigned DEFAULT NULL,
+  `id_tili` int unsigned NOT NULL,
+  PRIMARY KEY (`id_tilitapahtumat`),
+  UNIQUE KEY `id_tilitapahtumat_UNIQUE` (`id_tilitapahtumat`),
+  KEY `fk_tilitapahtumat_tili1_idx` (`id_tili`),
+  CONSTRAINT `id_tili_tilitapahtumat` FOREIGN KEY (`id_tili`) REFERENCES `tili` (`id_tili`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tilitapahtumat`
+--
+
+LOCK TABLES `tilitapahtumat` WRITE;
+/*!40000 ALTER TABLE `tilitapahtumat` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tilitapahtumat` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-03-29 13:05:16
