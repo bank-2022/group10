8afd0fcd3b8b877e797f0dc39290e6673c403f13
t1sasa03 <t1sasa03@students.oamk.fi>
2022-04-11 11:52:47 +0300

    webtoken lisätty

---
+++ api/routes/login.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const login = require('../models/login_model');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+router.post('/', 
+  function(request, response) {
+    if(request.body.korttinumero && request.body.pin){
+      const korttinumero = request.body.korttinumero;
+      const pin = request.body.pin;
+        login.checkPassword(korttinumero, function(dbError, dbResult) {
+          if(dbError){
+            response.json(dbError);
+          }
+          else{
+            if (dbResult.length > 0) {
+              bcrypt.compare(pin,dbResult[0].pin, function(err,compareResult) {
+                if(compareResult) {
+                  console.log("succes");
+                  const token = generateAccessToken({ korttinumero: korttinumero });
+                  response.send(token);
+                }
+                else {
+                    console.log("wrong pin");
+                    response.send(false);
+                }			
+              }
+              );
+            }
+            else{
+              console.log("card does not exists");
+              response.send(false);
+            }
+          }
+          }
+        );
+      }
+    else{
+      console.log("rfid or pin missing");
+      response.send(false);
+    }
+  }
+);
+
+function generateAccessToken(korttinumero) {
+    dotenv.config();
+    return jwt.sign(korttinumero, process.env.MY_TOKEN, { expiresIn: '1800s' });
+  }
+
+module.exports=router;
